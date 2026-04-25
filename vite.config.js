ddd8ab152b893f4bb4e6f673f8c7168a369437f3
talparico <talparico@gmail.com>
2026-04-25 11:54:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { sveltekit } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [svelte()]
 });
