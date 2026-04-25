5e64c251baac9046a5ca8da4ecb881400bab5977
talparico <talparico@gmail.com>
2026-04-25 11:40:49 +0200

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import '../app.css';
+  import Nav from '$lib/components/Nav.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+  import KontaktLeiste from '$lib/components/KontaktLeiste.svelte';
+</script>
+
+<Nav />
+
+<KontaktLeiste />
+
+<main id="main-content">
+  <slot />
+</main>
+
+<Footer />
