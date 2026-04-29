fc1b7a0b4d7b971c0eae1e692e8d43799b5804a6
talparico <talparico@gmail.com>
2026-04-29 06:55:58 +0200

    Update +page.js

--- src/routes/referenzen/+page.js
+++ src/routes/referenzen/+page.js
@@ -1,7 +1,8 @@
-import projekte from '../../content/projekte/projekte.json';
+import data from '../../content/projekte/projekte.json';
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+  const projekte = data.projekte || [];
   const sorted = [...projekte].sort((a, b) => (b.jahr || 0) - (a.jahr || 0));
   return { projekte: sorted };
 }
