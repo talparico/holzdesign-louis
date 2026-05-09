d084282330841c1d72928949db4ce846d2314fbb
talparico <talparico@gmail.com>
2026-05-09 10:17:49 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,10 @@
   import Nav from '$lib/components/Nav.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import KontaktLeiste from '$lib/components/KontaktLeiste.svelte';
+  import { page } from '$app/stores';
 </script>
 
-<Nav />
+<Nav currentPath={$page.url.pathname} />
 
 <KontaktLeiste />
 
