9206e1dad8c17e71cbd8246937a635ffefd912c1
talparico <talparico@gmail.com>
2026-05-27 08:22:16 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,10 @@ import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      edge: false,
-      split: false
-    }),
-    alias: {
-      $lib: './src/lib'
-    }
-  }
+	kit: {
+		adapter: adapter(),
+		trailingSlash: 'always'
+	}
 };
 
 export default config;
