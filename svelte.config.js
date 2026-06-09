7ab8f0825b0c53871a92398cb0f8bc61a64ea6c5
talparico <talparico@gmail.com>
2026-06-09 11:17:34 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		trailingSlash: 'always'
+		adapter: adapter()
 	}
 };
 
